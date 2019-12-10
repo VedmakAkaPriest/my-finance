d87565dea39bfd83ff912889cf2adf2d8b4224dd
Dmitriy Prilipko <vedmak5@gmail.com>
2019-12-10 21:04:45 +0200

    Initial setup

---
+++ storybook/stories/Button/index.web.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function Button({ onPress, children }) {
+  return <button onClick={onPress}>{children}</button>;
+}
+
+Button.defaultProps = {
+  children: null,
+  onPress: () => {},
+};
+
+Button.propTypes = {
+  children: PropTypes.node,
+  onPress: PropTypes.func,
+};
